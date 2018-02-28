7af0882d907e7c6fece11b2ebfcb8b246f43da18
John Barrett <barrett.m.john@googlemail.com>
2018-02-28 16:31:49 -0600

    Some optimisation ready for panel B

--- psem/figures/figure1.m
+++ psem/figures/figure1.m
@@ -23,16 +23,33 @@ if ~exist('successIndices','var') || isempty(successIndices)
     error('Try picking a less stupid mouse.');
 end
 
-firstSuccessIndex = successIndices(1);
-firstSuccessTime = timestamps(firstSuccessIndex);
-exampleTime = -0.499:0.001:0.5;
-exampleAngle = interp1(timestamps,angle,exampleTime+firstSuccessTime,'pchip');
+exampleTime = -0.249:0.001:0.25;
+exampleAngles = nan(numel(exampleTime),numel(successIndices));
+
+for ii = 1:size(exampleAngles,2)
+    ts = exampleTime+successTimes(ii);
+    
+    bad = false(size(ts));
+    
+    if ii > 1
+        bad = bad | ts <= successTimes(ii-1);
+        offset = angle(successIndices(ii-1));
+    else
+        offset = 0;
+    end
+    
+    if ii < numel(successIndices);
+        bad = bad | ts >= successTimes(ii+1);
+    end
+    
+    exampleAngles(~bad,ii) = interp1(timestamps,angle-offset,ts(~bad),'pchip');
+end
 
 %%
 
 figure
-plot(exampleTime,exampleAngle,'Color','b');
-line(repmat(xlim',1,2),threshold(firstSuccessIndex)*[-1 1; -1 1],'Color','r');
+plot(exampleTime,exampleAngles(:,1),'Color','b');
+line(repmat(xlim',1,2),threshold(successIndices(1))*[-1 1; -1 1],'Color','r');
 line([0 0],ylim,'Color','k','LineStyle','--');
 xlabel('Time from threshold crossing (s)');
 ylabel('Angle (degrees)');
@@ -40,3 +57,4 @@ legend({'Wheel' 'Threshold'});
 
 %%
 
+exampleAngles = zeros(numel(exampleAngle),numel(successIndices));
