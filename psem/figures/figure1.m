c5f91b234edf63ad5d3c928797223d2822cf0ecd
John Barrett <barrett.m.john@googlemail.com>
2018-03-05 16:43:03 -0600

    lalala

--- psem/figures/figure1.m
+++ psem/figures/figure1.m
@@ -1,64 +1,83 @@
 topFolder = 'Z:\LACIE\DATA\John\Videos\Behaviour';
-day = '20180219';
+firstDay = '20180219';
+lastDay = '20180223';
 cage = 'K156582';
 mouse = 'ted';
 
 %%
 
-cd(sprintf('%s\\%s\\%s',topFolder,day,cage));
+for day = {firstDay lastDay}
+    cd(sprintf('%s\\%s\\%s',topFolder,day{1},cage));
 
-%%
+    %%
 
-matFile = dir(sprintf('%s_session_1*.mat',mouse));
+    matFile = dir(sprintf('%s_session_1*.mat',mouse));
 
-if isempty(matFile)
-    error('Try picking a mouse that actually exists.');
-end
-
-load(matFile(1).name);
-
-%%
-
-if ~exist('successIndices','var') || isempty(successIndices)
-    error('Try picking a less stupid mouse.');
-end
-
-exampleTime = -0.249:0.001:0.25;
-exampleAngles = nan(numel(exampleTime),numel(successIndices));
-
-for ii = 1:size(exampleAngles,2)
-    ts = exampleTime+successTimes(ii);
-    
-    bad = false(size(ts));
-    
-    if ii > 1
-        bad = bad | ts <= successTimes(ii-1);
-        offset = angle(successIndices(ii-1));
-    else
-        offset = 0;
+    if isempty(matFile)
+        error('Try picking a mouse that actually exists.');
     end
-    
-    if ii < numel(successIndices);
-        bad = bad | ts >= successTimes(ii+1);
+
+    load(matFile(1).name);
+
+    %%
+
+    if ~exist('successIndices','var') || isempty(successIndices)
+        error('Try picking a less stupid mouse.');
     end
+
+    exampleTime = -0.249:0.001:0.25;
+    exampleAngles = nan(numel(exampleTime),numel(successIndices));
+
+    for ii = 1:size(exampleAngles,2)
+        ts = exampleTime+successTimes(ii);
+
+        bad = false(size(ts));
+
+        if ii > 1
+            bad = bad | ts <= successTimes(ii-1);
+            offset = angle(successIndices(ii-1));
+        else
+            offset = 0;
+        end
+
+        if ii < numel(successIndices);
+            bad = bad | ts >= successTimes(ii+1);
+        end
+
+        exampleAngles(~bad,ii) = interp1(timestamps,angle-offset,ts(~bad),'pchip');
+    end
+
+    %%
+
+    figure
+    plot(exampleTime,exampleAngles(:,1),'Color','b');
+    line(repmat(xlim',1,2),threshold(successIndices(1))*[-1 1; -1 1],'Color','r');
+    line([0 0],ylim,'Color','k','LineStyle','--');
+    xlabel('Time from threshold crossing (s)');
+    ylabel('Angle (degrees)');
+    legend({'Wheel' 'Threshold'});
+
+    %%
+
+    figure
+    plot(exampleTime,exampleAngles,'Color','b');
+    line([0 0],ylim,'Color','k','LineStyle','--');
+    xlabel('Time from threshold crossing (s)');
+    ylabel('Wheel angle relative to previous threshold crossing (degrees)');
+
+    %%
     
-    exampleAngles(~bad,ii) = interp1(timestamps,angle-offset,ts(~bad),'pchip');
+    figure
+    plot(timestamps/60,angle,'Color','b');
+    xlabel('Time (min)');
+    ylabel('Wheel angle (degrees)');
+
+    %%
+    
+    figure
+    plot(successTimes/60,learningCurve,'Color','b');
+    xlabel('Time (min)');
+    ylabel('# Rewards');
+
+    %%
 end
-
-%%
-
-figure
-plot(exampleTime,exampleAngles(:,1),'Color','b');
-line(repmat(xlim',1,2),threshold(successIndices(1))*[-1 1; -1 1],'Color','r');
-line([0 0],ylim,'Color','k','LineStyle','--');
-xlabel('Time from threshold crossing (s)');
-ylabel('Angle (degrees)');
-legend({'Wheel' 'Threshold'});
-
-%%
-
-figure
-plot(exampleTime,exampleAngles,'Color','b');
-line([0 0],ylim,'Color','k','LineStyle','--');
-xlabel('Time from threshold crossing (s)');
-ylabel('Wheel angle relative to previous threshold crossing (degrees)');
--- psem/psem_analysis.m
+++ psem/psem_analysis.m
@@ -1,5 +1,5 @@
-baseFolder = 'Z:\LACIE\DATA\John\cells\';
-summarySpreadsheet = [baseFolder 'psem experiments.xlsx'];
+baseFolders = {'Z:\LACIE\DATA\John\cells\' 'Z:\LACIE\DATA\Laurie\Patch\'};
+summarySpreadsheet = [baseFolders{1} 'psem experiments dedup.xlsx'];
 
 %%
 
@@ -29,6 +29,16 @@ for jj = 1:nConditions
         if isempty(txt{jj+1,kk+4})
             continue
         end
+        
+        if txt{jj+1,1}(1) == 'L'
+            traces{jj,kk} = txt{jj+1,kk+4};
+            
+            if isempty(strfind(traces{jj,kk},'.'))
+                traces{jj,kk} = [traces{jj,kk} '.h5'];
+            end
+            
+            continue
+        end
 
         % TODO : WaveSurfer
         tokens = regexp(txt{jj+1,kk+4},traceRegex,'tokens');
@@ -43,28 +53,32 @@ for jj = 1:nConditions
             lastTrace = str2double(traceParts{3});
         end
 
-        traces{jj,kk} = arrayfun(@(ll) sprintf('%s%s%04d.xsg',experiments{ii},prefix,ll),(firstTrace:lastTrace)','UniformOutput',false);
+        traces{jj,kk} = arrayfun(@(ll) sprintf('%s%s%04d.xsg',experiments{jj},prefix,ll),(firstTrace:lastTrace)','UniformOutput',false);
     end
 end
-%%
-experimentFolder = [baseFolder experiments{ii}];
 
-cd(experimentFolder);
 %%
 nCells = size(uniqueCells,1);
 
 seenIntrinsic = zeros(nCells,3);
 seenIsteps = zeros(nCells,3);
 
-Vm = cell(nCells,3,0);
-Rs = cell(nCells,3,0);
+allRsData = cell(nCells,3,0);
 
-rheobases = cell(nCells,3,0);
-fiCurves = cell(1,nCells,3,0);
-
-amplitudes = -200:50:400;
-
+allFIData = cell(nCells,3,0);
+allFISteps = cell(nCells,3,0);
+isEmpties = cell(nCells,3,0);
+sampleRates = zeros(nCells,1);
+%%
+amplitudes = -200:50:900;
+%%
 for jj = 1:nConditions
+    experiment = experiments{jj};
+    
+    baseFolder = baseFolders{(double(experiment(1))-double('J'))/2+1};
+    
+    cd([baseFolder experiment]);
+    
     cellIndex = cellIndices(jj);
     conditionIndex = find(strcmpi(conditions{jj},uniqueConditions));
 
@@ -72,75 +86,135 @@ for jj = 1:nConditions
         intrinsicIndex = seenIntrinsic(cellIndex,conditionIndex)+1;
         seenIntrinsic(cellIndex,conditionIndex) = intrinsicIndex;
 
-        [rsData,sampleRate] = concatenateEphusTraces(traces{jj,1},[],1);
+        [rsData,sampleRate] = concatenateTraces(traces{jj,1},[],1);
+        
+        if sampleRates(cellIndex) == 0 && sampleRate > 0
+            sampleRates(cellIndex) = sampleRate;
+        end
 
         rsData = preprocess(rsData,sampleRate,'Start',0,'Window',0.2,'AverageFun',@nanmedian,'FilterLength',7,'PreFilter',false,'PostFilter',true,'PostFilterFun',@nanmedian);
 
-        Rs{cellIndex,conditionIndex,intrinsicIndex} = calculateCellParameters(rsData,-0.005,sampleRate,'ResponseStart',0.2,'ResponseLength',0.1,'SteadyStateStart',0.4,'SteadyStateLength',0.3);
-
-%             figure;
-%             plot((1:size(rsData,1))/sampleRate,rsData);
-%             title(sprintf('Expt %s cell %d vsteps %s rep %d',sheets{ii},cellIndices(jj),conditions{jj},intrinsicIndex));            
+        allRsData{cellIndex,conditionIndex,intrinsicIndex} = rsData;
     end
 
     if ~isempty(traces{jj,3})
         istepIndex = seenIsteps(cellIndex,conditionIndex)+1;
         seenIsteps(cellIndex,conditionIndex) = istepIndex;
-
-        [fiData,sampleRate,~,isEmpty,headers] = concatenateEphusTraces(traces{jj,3},[],1);
-
-        figure;
-        plot((1:size(fiData,1))/sampleRate,fiData);
-        title(sprintf('Expt %s cell %d Isteps %s rep %d',experiments{ii},cellIndices(jj),conditions{jj},istepIndex)); 
-
-%             stimuli = extractEphusSquarePulseTrainParameters(headers(find(~cellfun(@isempty,{headers.ephusVersion}),1)),1,'program','pulseJacker');
-%             amplitudes = vertcat(stimuli.amplitude);
-
-        spikeIndices = findThresholdCrossings(fiData,0,'rising');
-
-        Vm{cellIndex,conditionIndex,istepIndex} = nanmean(nanmedian(fiData(1:(0.2*sampleRate),:),2));
-
-        if all(cellfun(@isempty,spikeIndices))
-            rheobase = Inf;
+        
+        if strncmpi(experiment,'J',1)
+            allFISteps{cellIndex,conditionIndex,istepIndex} = -200:50:400;
+            dataFiles = traces{jj,3};
         else
-            rheobase = amplitudes(find(cellfun(@numel,spikeIndices) > 0,1)); % TODO : interpolate?
+            dataFiles = {ws.loadDataFile(traces{jj,3})};
+            % get the stimuli from the dataFile, not the header
+            allFISteps{cellIndex,conditionIndex,istepIndex} = extractWavesurferSquarePulseTrainParameters(dataFiles{1},[],1);
         end
 
-        rheobases{cellIndex,conditionIndex,istepIndex} = rheobase;
-
-        fiCurve = cellfun(@(t) ternaryfun(isempty(t),@() 0,@() ternaryfun(numel(t) == 1,@() NaN,@() t(end)-t(1)/sampleRate)),spikeIndices)';
-        fiCurve(isEmpty) = NaN;
-        fiCurves{1,cellIndex,conditionIndex,istepIndex} = fiCurve;
+        [fiData,sampleRates(cellIndex,conditionIndex,istepIndex),~,isEmpties{cellIndex,conditionIndex,istepIndex},headers] = concatenateTraces(dataFiles,[],1);
+        
+        if sampleRates(cellIndex) == 0 && sampleRate > 0
+            sampleRates(cellIndex) = sampleRate;
+        end
+        
+        allFIData{cellIndex,conditionIndex,istepIndex} = fiData;
     end
 end
-
-close all
 %%
-missingIntrinsic = find(cellfun(@isempty,Rs))';
+Rs = cell(size(allRsData));
+Ri = cell(size(allRsData));
+Racc = cell(size(allRsData));
 
-for jj = missingIntrinsic
-    Rs{jj} = NaN;
+for ii = 1:numel(allRsData)
+    if isempty(allRsData{ii})
+        continue
+    end
+    
+    [cellIndex,~,~] = ind2sub(size(allRsData),ii);
+    [Rs{ii},Ri{ii},~,~,~,Racc{ii}] = calculateCellParameters(allRsData{ii},-0.005,sampleRates(cellIndex),'ResponseStart',0.2,'ResponseLength',0.1,'SteadyStateStart',0.4,'SteadyStateLength',0.3);
 end
 
-missingIsteps = find(cellfun(@isempty,Vm))';
+missing = cellfun(@isempty,Racc);
 
-for jj = missingIsteps
-    Vm{jj} = NaN;
-    rheobases{jj} = NaN;
-    fiCurves{jj} = NaN(13,1);
-end
-
+Rs(missing) = {NaN};
 Rs = cell2mat(Rs);
+
+Ri(missing) = {NaN};
+Ri = cell2mat(Ri);
+
+Racc(missing) = {NaN};
+Racc = cell2mat(Racc);
+%%
+Vm = cell(size(allFIData));
+rheobases = cell(size(allFIData));
+fiCurves = cell(size(allFIData));
+
+for ii = 1:numel(allFIData)
+    if isempty(allFIData{ii})
+        continue
+    end
+    
+    spikeIndices = findThresholdCrossings(allFIData{ii},0,'rising');
+
+    [cellIndex,~,~] = ind2sub(size(allRsData),ii);
+    Vm{ii} = nanmean(nanmedian(allFIData{ii}(1:(0.2*sampleRates(cellIndex)),:),2));
+
+    fiCurve = cellfun(@(t) ternaryfun(isempty(t),@() 0,@() ternaryfun(numel(t) == 1,@() NaN,@() sampleRates(cellIndex)*numel(t)/(t(end)-t(1)))),spikeIndices)';
+    
+    rheobase = allFISteps{ii}(find(fiCurve > 0,1));
+    
+    if isempty(rheobase)
+        rheobase = Inf;
+    end
+    
+    rheobases{ii} = rheobase;
+    
+    fiCurve(isEmpties{ii}) = NaN;
+    fiCurves{ii} = fiCurve;
+end
+
+Vm(cellfun(@isempty,Vm)) = {NaN};
+rheobases(cellfun(@isempty,rheobases)) = {NaN};
+
 Vm = cell2mat(Vm);
 rheobases = cell2mat(rheobases);
-fiCurves = cell2mat(fiCurves);
-%%
 
-good = ~any(reshape(Rs,nCells,[]) > 40,2);
-goodCells = uniqueCells(good);
+%%
+allRs = Rs;
+allVm = Vm;
+allRheobases = rheobases;
+allFICurves = fiCurves;
+
+isRejectionStrict = false;
+
+if isRejectionStrict
+    good = ~any(reshape(Rs,nCells,[]) > 40,2); %#ok<UNRCH>
+else
+    bad = find(Rs > 40);
+    Rs(bad) = NaN;
+    Vm(bad) = NaN;
+    rheobases(bad) = NaN;
+    fiCurves(bad) = {[]};
+    
+    good = ~all(isnan(reshape(Rs,nCells,[])),2);
+end
+
+goodCells = uniqueCells(good,:);
+Rs = Rs(good,:,:);
 Vm = Vm(good,:,:);
 rheobases = rheobases(good,:,:);
-fiCurves = fiCurves(:,good,:,:);
+fiCurves = fiCurves(good,:,:);
+
+%%
+
+relativeRheobase = bsxfun(@rdivide,rheobases,rheobases(:,1,:));
+meanRelativeRheobase = nanmean(nanmean(relativeRheobase,3),1);
+
+deltaRheobase = diff(rheobases,[],2);
+meanDeltaRheobase = nanmean(nanmean(deltaRheobase,3),1);
+
+gfpPositive = ~ismember(goodCells,[3 3],'rows');
+pPSEM = signtest(nanmean(deltaRheobase(gfpPositive,1,:),3));
+pWash = signtest(nanmean(deltaRheobase(gfpPositive,2,:),3));
 
 %%
 
@@ -160,22 +234,91 @@ set(gca,'XTick',1:3,'XTickLabel',uniqueConditions);
 xlim([0.5 3.5]);
 ylabel('Rheobase (pA)');
 %%
-nGoodCells = sum(good);
+goodIndices = find(good);
+nGoodCells = numel(goodIndices);
 
 figure;
 [rows,cols] = subplots(nGoodCells);
 
+colours = 'brg';
+
 for jj = 1:nGoodCells
     subplot(rows,cols,jj);
     hold on;
-    fiCurve = squeeze(nanmedian(fiCurves(:,jj,:,:),4));
 
     for kk = 1:3
-        isGood = ~isnan(fiCurve(:,kk));
-        plot(amplitudes(isGood),fiCurve(isGood,kk),'Marker','o');
+        for ll = 1:size(fiCurves,3)
+            fiCurve = fiCurves{jj,kk,ll};
+            
+            if isempty(fiCurve)
+                continue
+            end
+            
+            isFinite = isfinite(fiCurve);
+            plot(allFISteps{goodIndices(jj),kk,ll}(isFinite),fiCurve(isFinite),'Color',colours(kk),'Marker','o');
+            xlim([-200 900]);
+        end
+    end
+    
+    yy = ylim;
+    
+    for kk = 1:2
+        line([-150 50 50 -150; 50 50 -150 -150],([0.1 0.1 0.45 0.45; 0.1 0.45 0.45 0.1]+0.45*(2-kk))*yy(2),'Color','k');
+        
+        vv = allFIData{goodIndices(jj),kk,1}(:,1:min(13,size(allFIData{goodIndices(jj),kk,1},2)));
+        
+        if isempty(vv)
+            continue
+        end
+        
+        vv = yy(2)*(0.35*(vv-min(vv(:)))/(max(vv(:))-min(vv(:)))+0.1+0.45*(2-kk));
+        
+        xx = linspace(-150,50,size(vv,1));
+        
+        plot(xx,vv);
+        
+        text(-100,yy(2)*(0.5+0.45*(2-kk)),uniqueConditions{kk});
+        
+        ylim([0 yy(2)]);
     end
 
-    title(sprintf('Expt %s cell %d',experiments{ii},goodCells(jj)));
+    title(sprintf('Expt %s cell %d',uniqueExperiments{goodCells(jj,1)},goodCells(jj,2)));
     xlabel('Current Step (pA)');
     ylabel('Firing Rate (Hz)');
 end
+
+%%
+
+allSteps = bigUnion(allFISteps{:});
+meanFICurve = nan(numel(allSteps),3,size(allFICurves,1),size(allFICurves,3));
+
+for ii = goodIndices(gfpPositive)'
+    for jj = 1:3
+        for kk = 1:size(allFICurves,3)
+            if isempty(allFICurves{ii,jj,kk})
+                continue
+            end
+            
+            indices = ismember(allSteps,allFISteps{ii,jj,kk});
+            meanFICurve(indices,jj,ii,kk) = allFICurves{ii,jj,kk};
+        end
+    end
+end
+
+meanFICurve = nanmedian(reshape(meanFICurve,numel(allSteps),3,[]),3);
+
+figure
+hold on;
+
+for ii = 1:3
+    plot(allSteps(~isnan(meanFICurve(:,ii))),meanFICurve(~isnan(meanFICurve(:,ii)),ii),'Marker','o');
+end
+
+%%
+
+cd(baseFolders{1});
+saveas(gcf,'all_the_good_fi_curves','fig')
+
+%%
+
+save('psem_analysis.mat','traces','allRsData','allFIData','allFISteps','allSteps','meanFICurve','Rs','Vm','rheobases','fiCurves','allRs','allVm','allRheobases','allFICurves');
