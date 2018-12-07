e58e0b4efa1394ffd31a5426d722b28beaf94314
John Barrett <barrett.m.john@googlemail.com>
2018-12-07 16:28:26 -0600

    Plot response parameters

--- rhd/calculateLinearArrayResponseParams.m
+++ rhd/calculateLinearArrayResponseParams.m
@@ -219,4 +219,45 @@ function responseParams = calculateLinearArrayResponseParams(folder,varargin) % 
         
         set(findobj(gcf,'Type','Axes'),'YLim',yy);
     end
+    
+    nSubplots = size(plotData(1).peakAmplitudes,3); % don't use resultSize because it's not guaranteed to have enough elements
+    isParamsAsSubplots = nSubplots == 1;
+    
+    if isParamsAsSubplots
+        nSubplots = numel(fields);
+        nFigures = 1;
+    else
+        error('This hasn''t been tested yet.');
+        nFigures = numel(fields);
+    end
+    
+    [rows,cols] = subplots(nSubplots);
+    ylabels = {'Peak Amplitude (spikes/s)' 'Peak latency (ms)' '10-90% slope onset latency (ms)' 'Rise time (ms)' 'Fall time (ms)' 'FWHM (ms)' 'Time to >10% of peak (ms)' 'Time to >90% of peak (ms)' 'Time to <90% of peak (ms)' 'Time to <10% of peak (ms)' 'Time to >50% of peak (ms)' 'Time to <50% of peak (ms)' 'Baseline mean (spikes/s)' 'Baseline S.D. (spikes/s)' 'Threshold (spikes/s)' 'Threshold crossing latency - onset (ms)' 'Threshold crossing latency - offset (ms)' 'Response AUC (spikes)'};
+    coefficients = [1 1000*ones(1,11) 1 1 1 1000 1000 1];
+    
+    for ii = 1:nFigures
+        figure;
+        
+        for jj = 1:nSubplots
+            subplot(rows,cols,jj);
+            
+            fieldIndex = ternaryop(isParamsAsSubplots,jj,ii);
+            hs = plot(coefficients(fieldIndex)*plotData(1).(fields{fieldIndex})(:,:,ternaryop(isParamsAsSubplots,1,jj)));
+            
+            xlim([0.5 resultSize(1)+0.5]);
+            
+            ylabel(ylabels{fieldIndex});
+            
+            if jj == 1
+                if rows*cols > nSubplots
+                    a = subplot(rows,cols,nSubplots+1);
+                    hs = copyobj(hs,a);
+                    xlim(a,[-1 0]);
+                    set(a,'Visible','off');
+                end
+                
+                legend(hs,subplotNames,'Location','NorthWest');
+            end
+        end
+    end
 end
